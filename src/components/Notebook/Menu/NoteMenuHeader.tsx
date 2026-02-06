f3a9ad3e26aad0c5e46ee75e343b84eb0776373c
mikocst <mikocastillo7@gmail.com>
2026-02-05 21:10:03 -0600

    imported featherIcons and cleaned up astro config to utilize them.

--- src/components/Notebook/Menu/NoteMenuHeader.tsx
+++ src/components/Notebook/Menu/NoteMenuHeader.tsx
@@ -5,7 +5,7 @@ const NoteMenuHeader = () => {
   return (
     <div className = "flex flex-row justify-between items-center">
         <h1>Notes</h1>
-        <button className = "p-1 rounded-sm border border-gray-400">
+        <button className = "p-1 rounded-sm border border-gray-400 cursor-pointer">
             <Edit size={'16px'} className = "text-gray-400"/>
         </button>
     </div>
