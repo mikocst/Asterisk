9908a95e40ba042ebc034f97fc351e2330c03e76
mikocst <mikocastillo7@gmail.com>
2026-02-09 23:00:35 -0600

    Empty state created for noteArea

--- src/components/Notebook/NoteArea/NoteArea.tsx
+++ src/components/Notebook/NoteArea/NoteArea.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const NoteArea = () => {
   return (
-    <div>NoteArea</div>
+    <div className = "h-full w-full flex flex-col justify-center items-center gap-2">
+        <div className = "relative flex justify-center items-center px-12 py-10 bg-gray-50 rounded-full">
+            <div className = "w-12 h-16 bg-gray-200 rounded-md shadow-md absolute bottom-10 rotate-12 left-14"></div>
+            <div className = "w-12 h-16 bg-gray-300/60 rounded-md shadow-md"></div>
+        </div>
+        <p className = "text-black/40">Create or select a note.</p>
+      </div>
   )
 }
 
