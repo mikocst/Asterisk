f69f0746d76f5d857bff32b9dd8d8b2799d0f195
mikocst <mikocastillo7@gmail.com>
2026-02-11 22:51:32 -0600

    Incorporated base date functionality in the component.

--- src/components/Notebook/NoteArea/CreatedNoteDate.tsx
+++ src/components/Notebook/NoteArea/CreatedNoteDate.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Clock } from 'feather-icons-react'
 
 const CreatedNoteDate = () => {
+
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString('en-US', {month: 'short', day: 'numeric', year: 'numeric'})
+
   return (
     <div className = "flex flex-row justify-between">
             <div className = "flex flex-row gap-2">
                 <Clock size={"20px"} className = "text-black/50"/>
-                <p className = "text-black/50">Created at</p>
+                <p className = "text-black/50">Created at {formattedDate}</p>
             </div>
     </div>
   )
