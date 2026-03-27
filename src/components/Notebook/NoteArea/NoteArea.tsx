5f8566b5280e69f42fad90f5948183e70eac7c6f
mikocst <mikocastillo7@gmail.com>
2026-03-27 15:10:55 -0500

    toast initial animation completed

--- src/components/Notebook/NoteArea/NoteArea.tsx
+++ src/components/Notebook/NoteArea/NoteArea.tsx
@@ -2,9 +2,10 @@ import { useNotebook } from "../NotebookContext";
 import NoteAreaIdle from "./NoteAreaIdle";
 import NoteAreaActive from "./NoteAreaActive";
 import NoteDeletedToast from "./NoteDeletedToast";
+import { AnimatePresence, motion, time } from "motion/react";
 
 const NoteArea = () => {
-  const {creatingNote, activeNoteId} = useNotebook();
+  const {creatingNote, activeNoteId, showToast} = useNotebook();
 
   const isBusy = creatingNote || activeNoteId !== null;
 
@@ -13,9 +14,19 @@ const NoteArea = () => {
       {isBusy ? 
         <NoteAreaActive/> : <NoteAreaIdle/> 
       }
-      <div className = "absolute bottom-2 w-auto p-2 shadow-xl rounded-lg left-1/2 -translate-x-1/2 bg-white border border-black/20">
-        <NoteDeletedToast/>
-      </div>
+      <AnimatePresence>
+        {showToast &&
+        <motion.div
+          initial = {{opacity: 0, y: 50}}
+          animate = {{opacity: 1, y: 0}}
+          exit = {{opacity: 0, y: 20}}
+          transition = {{duration: 0.2}}
+          className = "absolute bottom-2 w-auto p-2 shadow-xl rounded-lg left-1/2 -translate-x-1/2 bg-white border border-black/20"
+        >
+            <NoteDeletedToast/>
+        </motion.div>
+        }
+      </AnimatePresence>
     </div>
   )
 }
