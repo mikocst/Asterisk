04664797e9090514ff4fbeeffc286baf0aeb8eed
mikocst <mikocastillo7@gmail.com>
2026-04-03 12:56:09 -0500

    Wired up favorite notes to component

--- src/components/Notebook/Menu/FavoriteNotes.tsx
+++ src/components/Notebook/Menu/FavoriteNotes.tsx
@@ -1,15 +1,31 @@
 import { useNotebook } from "../NotebookContext"
 
 const FavoriteNotes = () => {
-  const {notes} = useNotebook();
+  const {notes, handleNoteClick} = useNotebook();
 
-  const favoriteNotes = notes.filter((note) => note.isFavorited)
+  const favoriteNotes = notes.filter((note) => note.isFavorited);
 
   return (
     <div className = "flex flex-col gap-2">
         <h2 className = "text-lg font-medium text-gray-500">FAVORITES</h2>
         <div className = "w-full flex justify-center">
+            {favoriteNotes.length > 0 ? (
+              favoriteNotes.map((favNote) =>
+              <div
+              key = {favNote.id}
+              onClick={() => handleNoteClick(favNote.id)}
+              className = "flex flex-col border-b border-gray-300/70 w-full justify-center pb-1 cursor-pointer"
+              >
+                <h3 className = "text-black/50">{favNote.title}</h3>
+                <div className = "flex flex-row gap-1 text-sm text-black/30">
+                    <p>{favNote.createdAt}:</p>
+                    <p className = "truncate max-w-[17ch]">{favNote.content}</p>
+                </div>
+              </div>
+              )
+            ):
             <p className = "text-sm text-gray-400">No Notes Favorited</p>
+            }
         </div>
     </div>
   )
