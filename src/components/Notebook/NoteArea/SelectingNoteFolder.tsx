f2dd7552f0ead272d340a2cf9a3529fae349dfae
mikocst <mikocastillo7@gmail.com>
2026-04-08 23:58:20 -0500

    wired up handle folders to create a new folder when folder doesn't already exist in folders array

--- src/components/Notebook/NoteArea/SelectingNoteFolder.tsx
+++ src/components/Notebook/NoteArea/SelectingNoteFolder.tsx
@@ -17,7 +17,7 @@ const SelectingNoteFolder = () => {
   const filtered = folders.filter(f => f.title.toLowerCase().includes(query.toLowerCase()));
   const folderLabel = draft?.folder ? draft.folder : "Select a Folder";
   const showCreateOption = query.trim().length > 0 && !folders.some(f => f.title.toLowerCase() === query.toLowerCase());
-  const totalOptions = showCreateOption ? [...filtered, "CREATE_NEW"] : filtered;
+  const totalOptions = showCreateOption ? [...filtered, ""] : filtered;
 
   return (
     !isSearching ? (
@@ -39,11 +39,16 @@ const SelectingNoteFolder = () => {
          onChange = {(e) => setQuery(e.target.value)}
          className = "rounded-md border border-gray-300 px-2"
          />
-         <div className = "absolute top-full left-0 w-[19%] bg-white shadow-lg h-auto p-1 rounded-md">
+         <div className = "absolute top-full left-0 w-[20%] bg-white shadow-lg h-auto p-1 rounded-md">
             {totalOptions.map((option, index) =>{
              if( typeof option === "string") {
               return(
-                <div></div>
+                <div 
+                className = "cursor-pointer hover:bg-gray-200/50 rounded-md w-full py-1 px-2 flex items-start"
+                onClick={() => handleFolders(query)}
+                >
+                  <p>Create {query}</p>
+                </div>
               )
              }
              return(
