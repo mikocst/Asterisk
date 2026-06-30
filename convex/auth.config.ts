052c78e3e2763eb1a288be593e0a233548428a69
mikocst <mikocastillo7@gmail.com>
2026-06-30 16:25:38 -0500

    updating auth config again to not include hard coded URL

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,13 @@
+const domain = process.env.CLERK_JWT_ISSUER_DOMAIN;
+
+if (!domain) {
+  throw new Error("Missing CLERK_JWT_ISSUER_DOMAIN environment variable");
+}
+
 export default {
     providers: [
         {
-            domain: "https://clerk.asteriskprod.app",
+            domain: domain,
             applicationID: "convex"
         }
     ]
